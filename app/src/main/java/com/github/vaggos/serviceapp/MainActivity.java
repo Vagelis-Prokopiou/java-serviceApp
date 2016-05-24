74ebef68d6f847fc42b4da2bc99b68e68b2d2f43
Vagelis <drz4007@gmail.com>
2016-05-24 13:07:02 +0300

    I got all my widget references in the java file. Todo: Add clicklistener

--- app/src/main/java/com/github/vaggos/serviceapp/MainActivity.java
+++ app/src/main/java/com/github/vaggos/serviceapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.github.vaggos.serviceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the buttons.
+        Button btn_proceed = (Button) findViewById(R.id.btn_proceed);
+        Button btn_check = (Button) findViewById(R.id.btn_check);
+        Button btn_update = (Button) findViewById(R.id.btn_update);
+        Button btn_insert = (Button) findViewById(R.id.btn_insert);
+        Button btn_available = (Button) findViewById(R.id.btn_available);
+
+        // Get the Textview
+        TextView textview = (TextView) findViewById(R.id.textView);
+
+        // Set a listener to btn_proceed.
+//        btn_proceed.setOnClickListener();
+
+
+
     }
 }
