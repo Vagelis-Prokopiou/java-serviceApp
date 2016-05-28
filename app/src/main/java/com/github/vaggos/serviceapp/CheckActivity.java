daaf05ab4d0960e077cde64aef63c70bbb7d2f10
Vagelis <drz4007@gmail.com>
2016-05-28 17:28:49 +0300

    Updated "Run Check" activity layout.

--- app/src/main/java/com/github/vaggos/serviceapp/CheckActivity.java
+++ app/src/main/java/com/github/vaggos/serviceapp/CheckActivity.java
@@ -1,8 +1,10 @@
 package com.github.vaggos.serviceapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,15 +18,36 @@ public class CheckActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void alert(int global_kms) {
+        if (global_kms == 0) {
+            // Launch alert message
+            new AlertDialog.Builder(CheckActivity.this)
+                    .setTitle("Vehicle kilometres")
+                    .setMessage("Please provide the vehicle's total kilometres/miles to continue.")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Code here.
+                        }
+                    })
+//                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+//                                public void onClick(DialogInterface dialog, int which) {
+//                                    // do nothing
+//                                }
+//                            })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
     }
 
 }
