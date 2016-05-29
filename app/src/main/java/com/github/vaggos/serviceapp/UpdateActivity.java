281b5c614981961ec62db2842792d9ba32923f26
Vagelis <drz4007@gmail.com>
2016-05-29 12:20:26 +0300

    Removed the fragment form "Update" activity.

--- app/src/main/java/com/github/vaggos/serviceapp/UpdateActivity.java
+++ app/src/main/java/com/github/vaggos/serviceapp/UpdateActivity.java
@@ -1,9 +1,11 @@
 package com.github.vaggos.serviceapp;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
+import android.view.View;
 
 public class UpdateActivity extends AppCompatActivity {
 
@@ -14,19 +16,7 @@ public class UpdateActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        // Get the data passed from the main activity.
-        Bundle extras = getIntent().getExtras();
-        String dataList = null;
-        if (extras != null) {
-            dataList = extras.getString("dataList");
-        }
-
-        // Get the text field.
-        TextView textView_test = (TextView) findViewById(R.id.textView_test);
-        textView_test.setText(dataList);
     }
 
 }
