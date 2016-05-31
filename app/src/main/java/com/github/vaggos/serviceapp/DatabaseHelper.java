ae1f0edab4bc7e826ddb009364c936cb359ac683
Vagelis <drz4007@gmail.com>
2016-05-31 18:04:31 +0300

    Database created.

--- app/src/main/java/com/github/vaggos/serviceapp/DatabaseHelper.java
+++ app/src/main/java/com/github/vaggos/serviceapp/DatabaseHelper.java
@@ -5,17 +5,35 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public static final String DATABASE_NAME = "service.db";
+    public static final String TABLE_NAME = "service_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "SPARE_PART";
+    public static final String COL_3 = "DATE_CHANGED";
+    public static final String COL_4 = "DATE_INTERVAL";
+    public static final String COL_5 = "KMS_CHANGED";
+    public static final String COL_6 = "KMS_INTERVAL";
+
+
+    public DatabaseHelper(Context context) {
+//        super(context, name, factory, version);
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table " + TABLE_NAME + " (" +
+                COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL_2 + " TEXT, " +
+                COL_3 + " TEXT, " +
+                COL_4 + " INTEGER, " +
+                COL_5 + " INTEGER, " +
+                COL_6 + " INTEGER)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("drop table if exists " + DATABASE_NAME);
     }
 }
