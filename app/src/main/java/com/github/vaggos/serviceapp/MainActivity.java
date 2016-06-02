fb77c6a6a8d8f9896bc15b1c7ae1647d8fcd747c
Vagelis <drz4007@gmail.com>
2016-06-02 20:07:12 +0300

    Code formatting.

--- app/src/main/java/com/github/vaggos/serviceapp/MainActivity.java
+++ app/src/main/java/com/github/vaggos/serviceapp/MainActivity.java
@@ -1,8 +1,5 @@
 package com.github.vaggos.serviceapp;
 
-// Todo: Add scrollview to available activity.
-// Todo: Add scrollview to check activity.
-
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -10,12 +7,11 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Create a database variable.
     DatabaseHelper db = new DatabaseHelper(MainActivity.this);
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
