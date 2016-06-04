e056d017156415822b433614ca8d95b9aa68f9d5
Vagelis <drz4007@gmail.com>
2016-06-04 16:00:24 +0300

    MainActivity: Adding Donate button.

--- app/src/main/java/com/github/vaggos/serviceapp/MainActivity.java
+++ app/src/main/java/com/github/vaggos/serviceapp/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         Button btn_available = (Button) findViewById(R.id.btn_available);
         Button btn_delete = (Button) findViewById(R.id.btn_delete);
         Button btn_about = (Button) findViewById(R.id.btn_about);
+        Button btn_donate = (Button) findViewById(R.id.btn_donate);
 
         // Get all the data from the database.
         Cursor result = db.getAllData();
@@ -112,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Set a listener to btn_update.
+        // Set a listener to btn_about.
         btn_about.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -120,6 +121,18 @@ public class MainActivity extends AppCompatActivity {
                         // Launch the About Activity.
                         Intent aboutIntent = new Intent(MainActivity.this, AboutActivity.class);
                         startActivity(aboutIntent);
+                    }
+                }
+        );
+
+        // Set a listener to btn_about.
+        btn_donate.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Launch the About Activity.
+                        Intent donateIntent = new Intent(MainActivity.this, DonateActivity.class);
+                        startActivity(donateIntent);
                     }
                 }
         );
