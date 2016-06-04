160940adbea1bb3b07e382e770fb5dc3a64538fc
Vagelis <drz4007@gmail.com>
2016-06-04 14:48:01 +0300

    AboutActivity: Adding code for Donate button.

--- app/src/main/java/com/github/vaggos/serviceapp/AboutActivity.java
+++ app/src/main/java/com/github/vaggos/serviceapp/AboutActivity.java
@@ -1,11 +1,13 @@
 package com.github.vaggos.serviceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -16,6 +18,21 @@ public class AboutActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Get the Donate button.
+        Button btn_donate_about = (Button) findViewById(R.id.btn_donate_about);
+
+        // Set listener to btn_donate_about button.
+        btn_donate_about.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Launch the About Activity.
+                        Intent donateIntent = new Intent(AboutActivity.this, DonateActivity.class);
+                        startActivity(donateIntent);
+                    }
+                }
+        );
     }
 
 }
