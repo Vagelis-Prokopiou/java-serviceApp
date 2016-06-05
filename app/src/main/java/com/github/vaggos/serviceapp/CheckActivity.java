34b34622a4fe7e4c88877d81a3d69e5463860da8
Vagelis <drz4007@gmail.com>
2016-06-05 11:56:43 +0300

    Code refactoring.

--- app/src/main/java/com/github/vaggos/serviceapp/CheckActivity.java
+++ app/src/main/java/com/github/vaggos/serviceapp/CheckActivity.java
@@ -65,78 +65,74 @@ public class CheckActivity extends AppCompatActivity {
         final EditText editText_total_kms = (EditText) findViewById(R.id.editText_total_kms);
 
         // Set a listener to btn_proceed.
-        btn_proceed.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                // If a value has been provided execute try.
-                try {
-                    // Get the value of the text field.
-                    int val = Integer.parseInt(editText_total_kms.getText().toString());
-                    // Set the global_kms variable.
-                    CheckActivity.global_kms = val;
-                } catch (NumberFormatException e) {/* Code here if necessary. */}
-
-                // Run the check.
-                // Prepare the message that will be displayed.
-                String message = "Results:\n\n";
-                // Check if the MainActivity.global_kms has been set.
-                if (CheckActivity.global_kms > 0) {
-                    // Loop over your data.
-                    for (int i = 1; i < dataList.length; i++) {
-                        // Create the local variables.
-                        String spare_part = dataList[i][0];
-                        int kms_changed = Integer.parseInt(dataList[i][3].toString());
-                        int kms_interval = Integer.parseInt(dataList[i][4].toString());
-                        // Build the date_changed date.
-                        String dateChanged = dataList[i][1].toString();
-                        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-                        Date date_changed = null;
+        btn_proceed.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // If a value has been provided execute try.
                         try {
-                            date_changed = format.parse(dateChanged);
-                        } catch (ParseException e) {
-                            e.printStackTrace();
-                        }
-                        // Build the date_interval date.
-                        int dateInterval = Integer.parseInt(dataList[i][2].toString());
-                        Calendar calendar = Calendar.getInstance();
-                        calendar.setTime(date_changed);
-                        calendar.add(Calendar.MONTH, dateInterval);
-                        Date date_interval = calendar.getTime();
+                            // Get the value of the text field.
+                            int val = Integer.parseInt(editText_total_kms.getText().toString());
+                            // Set the global_kms variable.
+                            CheckActivity.global_kms = val;
+                        } catch (NumberFormatException e) {/* Code here if necessary. */}
 
-                        // Check the kms.
-                        if (CheckActivity.global_kms - kms_changed >= kms_interval) {
-                            // Build the message.
-                            message += spare_part + " kms status:\nExceeded the allowed " + kms_interval + " kms between changes, for " + (CheckActivity.global_kms - kms_changed) + " kms.\n\n";
-                        }
+                        // Run the check.
+                        // Prepare the message that will be displayed.
+                        String message = "Results:\n\n";
+                        // Check if the MainActivity.global_kms has been set.
+                        if (CheckActivity.global_kms > 0) {
+                            // Loop over your data.
+                            for (int i = 1; i < dataList.length; i++) {
+                                // Create the local variables.
+                                String spare_part = dataList[i][0];
+                                int kms_changed = Integer.parseInt(dataList[i][3].toString());
+                                int kms_interval = Integer.parseInt(dataList[i][4].toString());
+                                // Build the date_changed date.
+                                String dateChanged = dataList[i][1].toString();
+                                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                                Date date_changed = null;
+                                try {
+                                    date_changed = format.parse(dateChanged);
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                                // Build the date_interval date.
+                                int dateInterval = Integer.parseInt(dataList[i][2].toString());
+                                Calendar calendar = Calendar.getInstance();
+                                calendar.setTime(date_changed);
+                                calendar.add(Calendar.MONTH, dateInterval);
+                                Date date_interval = calendar.getTime();
 
-                        // Check the dates.
-                        if (today.after(date_interval)) {
-                            // Format the date to ISO, for printing.
-                            SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd");
-                            String stringDate = date_format.format(date_interval);
-                            message += spare_part + " months status:\nExceeded the allowed " + dateInterval + " months between changes. It should have been changed on " + stringDate + ".\n\n";
+                                // Check the kms.
+                                if (CheckActivity.global_kms - kms_changed >= kms_interval) {
+                                    // Build the message.
+                                    message += spare_part + " kms status:\nExceeded the allowed " + kms_interval + " kms between changes, for " + (CheckActivity.global_kms - kms_changed) + " kms.\n\n";
+                                }
+
+                                // Check the dates.
+                                if (today.after(date_interval)) {
+                                    // Format the date to ISO, for printing.
+                                    SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd");
+                                    String stringDate = date_format.format(date_interval);
+                                    message += spare_part + " months status:\nExceeded the allowed " + dateInterval + " months between changes. It should have been changed on " + stringDate + ".\n\n";
+                                }
+                            }
+                            // Print the result.
+                            textView_results.setText(message);
+                        } else {
+                            // The CheckActivity.global_kms has not been provided. Show alert message.
+                            new AlertDialog.Builder(CheckActivity.this)
+                                    .setTitle("Vehicle kilometres")
+                                    .setMessage("Please provide the vehicle's total kilometres/miles to continue.")
+                                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {/*Code here*/}
+                                    })
+                                    .setIcon(android.R.drawable.ic_dialog_alert)
+                                    .show();
                         }
                     }
-                    // Print the result.
-                    textView_results.setText(message);
-                } else {
-                    // The CheckActivity.global_kms has not been provided. Show alert message.
-                    alert(CheckActivity.global_kms);
                 }
-            }
-        });
-    }
-
-    public void alert(int global_kms) {
-        if (global_kms == 0) {
-            // Launch alert message
-            new AlertDialog.Builder(CheckActivity.this)
-                    .setTitle("Vehicle kilometres")
-                    .setMessage("Please provide the vehicle's total kilometres/miles to continue.")
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {/*Code here*/}
-                    })
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-        }
+        );
     }
 }
